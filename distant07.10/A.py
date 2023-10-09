8021a3f4d3559c4e02416a0ab14e22638fd851b9
onChill <99094236+onChill@users.noreply.github.com>
2023-10-09 16:20:27 +0500

    Update A.py

--- distant07.10/A.py
+++ distant07.10/A.py
@@ -1,11 +1,6 @@
-def even_or_odd(numbers):
-    evenNumbers = []
-    oddNumbers = []
-    for i in numbers:
-        if i % 2 == 0:
-            evenNumbers.append(i)
-        else:
-            oddNumbers.append(i)
-    print('even numbers: ' + String(evenNumbers))
-    print('even numbers: ' + String(oddNumbers))
-even_or_odd([1, 2, 4])
+def even_or_odd(number):
+    if number % 2 == 0:
+        return 'even'
+    else:
+        return 'odd'
+print(even_or_odd(2))
